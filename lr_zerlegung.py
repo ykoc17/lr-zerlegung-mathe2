56d08590afd173bd67611d50551a45a87116262a
Yagiz Koc <yagizkoc@yahoo.com.tr>
2023-05-30 13:25:57 +0200

    function implementations done

--- lr_zerlegung.py
+++ lr_zerlegung.py
@@ -11,7 +11,7 @@ def lr_ohne(A):
         pivot = R[i][i]
         for i_cont in range(i+1, len(R)):
             num = R[i_cont][i]
-            if (num == 0): continue
+            if (pivot == 0): continue
             else:
                 l_i = -(num / pivot)
                 L[i_cont][i] = -l_i
@@ -22,31 +22,33 @@ def lr_ohne(A):
 def lr_mit(A):
     n = len(A)
     P = np.identity(n) #Permutationsmatrix
-    L = np.zeros((n, n))
-    for p in range(n): L[p][p] = 1
-    R = A
+    L = np.identity(n)
+    R = A.copy()
 
-        #Spaltenpivotisierung ausrechnen
-        #betragsmäßig größte Spaltenelement herausfinden
-        #Zeilen vertauschen
-        #Die Nummer der vertauschten Zeilen in P speichern indem die korrespondierenden Spalten vertauscht werden
     for i in range(n):
+
+        absR_row_i = np.abs(R[:,i])
+        sliced_absR_row_i = absR_row_i[i:]
+        index_maxabsR_row_i = np.argmax(sliced_absR_row_i)
+        if index_maxabsR_row_i != 0:
+            P[:, [0, index_maxabsR_row_i]] = P[:, [index_maxabsR_row_i, 0]]
+            R[[0, index_maxabsR_row_i]] = R[[index_maxabsR_row_i, 0]]
+
         pivot = R[i][i]
         for i_cont in range(i + 1, len(R)):
             num = R[i_cont][i]
-            if (num == 0):
+            if (pivot == 0):
                 continue
             else:
                 l_i = -(num / pivot)
                 L[i_cont][i] = -l_i
                 for j_cont in range(i, len(R)):
-                    R[i_cont][j_cont] = R[i][j_cont] * l_i + R[i_cont][j_cont]
-    return L, R
+                    R[i_cont][j_cont] += R[i][j_cont] * l_i
+    return L, R, P
 
 def linsolve_ohne(A, b):
     n = len(A)
     L, R = lr_ohne(A)
-    print(L)
     x,  x_s = np.zeros(n), np.zeros(n)
 
     #Vorwärtseinsetzen
@@ -61,37 +63,52 @@ def linsolve_ohne(A, b):
     for i in reversed(range(0, n-1)):
         sum = 0
         for k in range(1, n-i): sum += x[n-k]*R[i][n-k]
-        x[i] = x_s[i] - sum/R[i][i]
+        x[i] = (x_s[i] - sum)/R[i][i]
 
     return x
 
 def linsolve_mit(A, b):
-    L, R = lr_mit(A)
-    x = np.empty(len(A))
+    n = len(A)
+    L, R, P = lr_mit(A)
+    x,  x_s = np.zeros(n), np.zeros(n)
+    Pb = np.dot(P, b)
+
+    #Vorwärtseinsetzen
+    x_s[0] = Pb[0]
+    for i in range(1, n):
+        sum = 0
+        for k in range(i): sum += x_s[k]*L[i][k]
+        x_s[i] = Pb[i] - sum
+
+    #Rückwärtseinsetzen
+    x[n-1] = x_s[n-1]/R[n-1][n-1]
+    for i in reversed(range(0, n-1)):
+        sum = 0
+        for k in range(1, n-i): sum += x[n-k]*R[i][n-k]
+        x[i] = (x_s[i] - sum)/R[i][i]
+
     return x
 
 #a)
-A = np.array([[1, 1, 3],
-              [1, 2, 2],
-              [2, 1, 5]])
+A = np.array([[1., 1., 3.],
+              [1., 2., 2.],
+              [2., 1., 5.]])
 
-b = np.array([2, 1, 1])
+b = np.array([2., 1., 1.])
 
-print(A)
-L, R = lr_ohne(A)
+L, R, P = lr_mit(A)
 print(L)
 print(R)
-print("")
-x = linsolve_ohne(A, b)
-print(x)
+print(P)
+print(linsolve_mit(A, b))
 
 #b)
-A = np.array([[0, 1, 0, -1],
-              [1, 2, 2, 1 ],
-              [1, 1, 1, 1 ],
-              [2, 1, -1, 2]])
+A = np.array([[0., 1., 0., -1.],
+              [1., 2., 2., 1. ],
+              [1., 1., 1., 1. ],
+              [2., 1., -1., 2.]])
 
-b = np.array([2, -1, -2, -11])
+b = np.array([2., -1., -2., -11.])
 
 #c)
 
